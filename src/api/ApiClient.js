f9cd7d756545a6d5c689abace42ebb99736240f8
Priyanshu Ranjan <heyapriyanshu@gmail.com>
2024-06-19 17:43:05 +0530

    ``

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const apiClient = axios.create(
     {//todolistbackend-production-d4f8.up.railway.app
-        baseURL: 'https://todolistbackend-production-d4f8.up.railway.app'
+        baseURL: 'http://3.108.40.166:8080'
         // baseURL: 'http://localhost:8080'
     }
 );
