84a1cc53a20f1c9080fd7ab0ceb439897e1b20cc
Priyanshu Ranjan <heyapriyanshu@gmail.com>
2024-06-19 23:46:48 +0530

    `

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -37,7 +37,7 @@ export default function TodoApp() {
                 <Route path='/' element={<LoginComponent />}></Route>
                 <Route path='/login' element={<LoginComponent />}></Route>
                         
-                        <Route path='/welcome/:username' element={
+                        <Route path='/welcome' element={
                             <AuthenticatedRoute>
                                 <HeaderComponent />
                                 <WelcomeComponent />
